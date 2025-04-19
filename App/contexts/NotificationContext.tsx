d22022473ce92d0013eb5b2b06fbc8e252bf5adb
Shreyash Galgale <shreyasgalgale019@gmail.com>
2025-04-19 13:49:17 +0530

    expoToken setup

--- App/contexts/NotificationContext.tsx
+++ App/contexts/NotificationContext.tsx
@@ -1,15 +1,64 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { View, Button, Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import axios from 'axios';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 const NotificationContext = createContext<any>(null);
 
 export const NotificationProvider = ({ children }: { children: React.ReactNode }) => {
-  const [fcmToken, setFcmToken] = useState('');
+  const [expoToken, setExpoToken] = useState<string | null>('');
   const [notifications, setNotifications] = useState<any[]>([]);
-  const [unreadCount, setUnreadCount] = useState(0);
+  const [unreadCount, setUnreadCount] = useState<number>(0);
+  async function registerForPushNotificationsAsync() {
+    if (!Device.isDevice) {
+      alert('Must use physical device');
+      return;
+    }
+
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Permission not granted');
+      return;
+    }
+
+    const tokenData = await Notifications.getExpoPushTokenAsync();
+    return tokenData.data;
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      if (token) {
+        setExpoToken(token);
+        console.log('expo token: ', token);
+      }
+    });
+  }, []);
 
   return (
     <NotificationContext.Provider
-      value={{ fcmToken, setFcmToken, notifications, setNotifications, unreadCount, setUnreadCount }}
+      value={{
+        expoToken,
+        setExpoToken,
+        notifications,
+        setNotifications,
+        unreadCount,
+        setUnreadCount,
+      }}
     >
       {children}
     </NotificationContext.Provider>
