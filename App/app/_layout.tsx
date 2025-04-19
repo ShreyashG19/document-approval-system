5ceb171d122c4f8990ebb011d9220323f8e9db61
Shreyash Galgale <shreyasgalgale019@gmail.com>
2025-04-19 13:49:17 +0530

    expoToken setup

--- App/app/_layout.tsx
+++ App/app/_layout.tsx
@@ -1,16 +1,17 @@
 import { Slot } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
 import { AuthProvider } from '../contexts/AuthContext'; // ✅ Make sure path is correct
+import { NotificationProvider } from '../contexts/NotificationContext';
 import './global.css'; // tailwind css
 
 export default function RootLayout() {
   return (
     <PaperProvider>
-      <AuthProvider>
-        <Slot />
-      </AuthProvider>
-    </ PaperProvider>
+      <NotificationProvider>
+        <AuthProvider>
+          <Slot />
+        </AuthProvider>
+      </NotificationProvider>
+    </PaperProvider>
   );
 }
-
-
