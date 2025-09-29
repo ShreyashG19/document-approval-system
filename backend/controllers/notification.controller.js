35b67d93571907dceda1e33ae29589683243105f
ShreyashG19 <shreyasgalgale019@gmail.com>
2025-09-29 22:24:44 +0530

    api response standardized

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,5 +1,7 @@
 const Notification = require("../models/notification.model");
 const asyncHandler = require("../utils/asyncHandler");
+const createApiError = require("../utils/createApiError");
+const ApiResponse = require("../utils/ApiResponse");
 
 const getNotifications = asyncHandler(async (req, res, next) => {
     await req.session.populate("user");
@@ -9,18 +11,12 @@ const getNotifications = asyncHandler(async (req, res, next) => {
     })
         .populate("to", "fullName username")
         .sort({ date: -1 });
-    if (notifications.length === 0) {
-        return res.status(200).json({
-            status: true,
-            message: "Notifications fetched successfully",
+
+    return res.status(200).json(
+        new ApiResponse(200, "Notifications fetched successfully", {
             notifications,
-        });
-    }
-    return res.status(200).json({
-        status: true,
-        message: "Notifications fetched successfully",
-        notifications,
-    });
+        }),
+    );
 });
 
 const markNotificationsAsSeen = asyncHandler(async (req, res, next) => {
@@ -32,10 +28,9 @@ const markNotificationsAsSeen = asyncHandler(async (req, res, next) => {
         },
         { $set: { seen: true } },
     );
-    return res.status(200).json({
-        status: true,
-        message: "Notifications marked as seen",
-    });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Notifications marked as seen"));
 });
 
 module.exports = {
